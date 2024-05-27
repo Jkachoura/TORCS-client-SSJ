6381d14fff9b262b169d38e0b452b0445652f4af
Sam Yost <90903248+samyost1@users.noreply.github.com>
2024-05-27 22:24:41 +0200

    Added manual gear shifting

--- client/my_driver.py
+++ client/my_driver.py
@@ -1,5 +1,6 @@
 import glob
 import os
+import time
 
 import pandas as pd
 from pytocl.driver import Driver
@@ -27,11 +28,15 @@ class MyDriver(Driver):
             self.scaler = pickle.load(f)
         
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.model = CarControlModel(68, 4).to(self.device)
+        self.model = CarControlModel(67, 3).to(self.device)
 
         latest_model_file = max(glob.glob('../models/model_*.pth'), key=os.path.getctime)
         self.model.load_state_dict(torch.load(latest_model_file))
         self.model.eval()
+
+        self.reverse_start_time = None  # Initialize reverse start time
+        self.reverse_duration_limit = 2  # Max duration in reverse in seconds
+
 
     def clip(self, v, lo, hi):
         if v < lo:
@@ -53,7 +58,7 @@ class MyDriver(Driver):
         features[' DistanceCovered'] = [carstate.distance_raced]
         # features['lastLapTime'] = [carstate.last_lap_time]
         # TODO remove gear input and maybe output for manual gear shifting logic (Test if it works better without it)
-        features[' Gear'] = [carstate.gear]
+        # features[' Gear'] = [carstate.gear]
         features[" Opponent_1"] = [carstate.opponents[0]]
         for i in range(1, 36):
             features[f'Opponent_{i+1}'] = [carstate.opponents[i]]
@@ -92,16 +97,9 @@ class MyDriver(Driver):
         command.steering = steer 
         command.brake = brake
 
-        gear = self.convert_gear_value_back(prediction[0][3])
-        command.gear = round(gear)
+        # gear = self.convert_gear_value_back(prediction[0][3])
+        # command.gear = round(gear)
 
-        # Gear shifting logic for reverse
-        if -10 < carstate.speed_x / MPS_PER_KMH < 5 and carstate.distance_raced > 10:
-            # If 50% of distance from edge values are <= 0.5
-            if len([x for x in carstate.distances_from_edge if x <= 5]) >= 9:
-                print("REVERSE")
-                print(carstate.distances_from_edge)
-                command.gear = -1
 
         # TODO with this manual gear shifting below enabled the car doesn't drive good
         # Remove gear from the model input and output and check difference in lap finishing time and driving
@@ -109,18 +107,43 @@ class MyDriver(Driver):
         # print(carstate.speed_x / MPS_PER_KMH)
         # Gear shifting logic
         # if gear != -1:
-        #     if carstate.gear > 0:
-        #         if carstate.rpm > self.RPM_UPSHIFT and carstate.gear < 6:
-        #             command.gear = carstate.gear + 1
-        #         elif carstate.rpm < self.RPM_DOWNSHIFT and carstate.gear > 1:
-        #             command.gear = carstate.gear - 1
-        #         else:
-        #             command.gear = carstate.gear
-        #     # else:
-        #     #     command.gear = 1  # Start in first gear if neutral or reverse
-        # else:
-        #     command.gear = gear
+        if carstate.gear != 0:
+            if carstate.rpm > self.RPM_UPSHIFT and carstate.gear < 6:
+                command.gear = carstate.gear + 1
+            elif carstate.rpm < self.RPM_DOWNSHIFT and carstate.gear > 1:
+                command.gear = carstate.gear - 1
+            else:
+                command.gear = carstate.gear
+        else:
+            command.gear = 1  # Start in first gear if neutral
 
+        # Check if should reverse
+        # Set reverse time at current time
+        # For a few seconds should reverse
+            
+
+        if -10 < carstate.speed_x / MPS_PER_KMH < 5 and carstate.distance_raced > 10:
+            if len([x for x in carstate.distances_from_edge if x <= 5]) >= 9:
+                if self.reverse_start_time is None:
+                    self.reverse_start_time = time.time()
+                    command.gear = -1
+                elif time.time() - self.reverse_start_time <= self.reverse_duration_limit:
+                    command.gear = -1
+                elif time.time() - self.reverse_start_time <= self.reverse_duration_limit + 4:
+                    command.gear = 1
+                else:
+                    self.reverse_start_time = None
+        # else:
+        #     self.reverse_start_time = None
+
+    #    # Logic to handle reverse gear
+    #     if carstate.gear == -1:
+    #         if self.reverse_start_time is None:
+    #             self.reverse_start_time = time.time()
+    #         elif time.time() - self.reverse_start_time > self.reverse_duration_limit:
+    #             command.gear = 1  # Switch to first gear after reverse duration limit
+    #     else:
+    #         self.reverse_start_time = None
 
 
         # print(f"Accelerator={accel},{prediction[0][0]:.2f},\tBrake={prediction[0][1]:.2f},\tSteer={prediction[0][2]:.2f},\tgear={gear}")
